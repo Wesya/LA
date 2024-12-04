14436eb8eaae0a043ed1ad0e6fc3561395d1474d
Wesya <wesya@student.gunadarma.ac.id>
2024-12-04 21:19:39 +0700

    first commit

---
+++ mahasiswa_mvs/src/main/java/com/mycompany/mahasiswa_mvs/Mahasiswa_mvs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_mvs;
+
+/**
+ *
+ * @author Ega
+ */
+public class Mahasiswa_mvs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ mahasiswa_orm/src/main/java/com/mahasiswa/controller/MahasiswaController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mahasiswa.controller;
+
+import com.mahasiswa.model.ModelMahasiswa;
+import java.util.List;
+
+/**
+ *
+ * @author Ega
+ */
+public interface MahasiswaController {
+    public void addMhs(ModelMahasiswa mhs);
+    
+    public ModelMahasiswa getMhs(int id);
+    
+    public void updateMhs(ModelMahasiswa mhs);
+    public void deleteMhs(int id);
+    
+    public List<ModelMahasiswa> getAllMahasiswa();
+}
